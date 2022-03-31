b19364cb234d665faad77d1810fff91583d73a50
gerkbyrd <40070813+gerkbyrd@users.noreply.github.com>
2022-03-31 14:31:53 +0200

    Update main_moco.py

--- Baselines/Originals/moco-main/moco-main/main_moco.py
+++ Baselines/Originals/moco-main/moco-main/main_moco.py
@@ -32,6 +32,7 @@ model_names = sorted(name for name in models.__dict__
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
 parser.add_argument('data', metavar='DIR',
                     help='path to dataset')
+#MAYBE USE THIS VERSION OF RESNET: https://github.com/kuangliu/pytorch-cifar/blob/master/models/resnet.py
 parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet50',
                     choices=model_names,
                     help='model architecture: ' +
